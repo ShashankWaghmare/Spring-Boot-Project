884bddd5d3af6ac0b63af3dc291e61eeee40e412
Shashank Waghmare <shashankplwwaghmare@gmail.com>
2024-05-05 09:50:38 +0530

    First Service Implementation.

--- BankApplication/src/main/java/com/example/demo/controller/RequestController.java
+++ BankApplication/src/main/java/com/example/demo/controller/RequestController.java
@@ -3,17 +3,21 @@ package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.Account;
 import com.example.demo.service.AccountService;
 
+@RestController
+@RequestMapping("/account")
 public class RequestController {
            
 	@Autowired
 	AccountService service;
 	
 	
-	@PostMapping("/account")
+	@PostMapping("/create")
 	public Account createAccount(@RequestBody Account account) {
 		Account createdAccount=service.createAccount(account);
 		return createdAccount;
